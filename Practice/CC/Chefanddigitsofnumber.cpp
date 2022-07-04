04c7de59cb6a09eae2a591ae0baeab50b21e1b62
0kopfkino3 <deepanshu.dr10@gmail.com>
2022-07-04 20:50:30 +0530

    Contest and Practice

---
+++ Practice/CC/Chefanddigitsofnumber.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define pb push_back
+#define pp pop_back
+#define Vec vector<long long int>
+const long long int MOD = 1e9+7;
+// Prime function with O(N^(1/2)) approach
+bool prime(int n){
+    int i=2;
+    if(n<2){
+        return false;
+    }
+    while(i*i<=n){
+        if(n%i==0){
+            return false;
+        }
+        i++;
+    }
+    return true;
+}
+// This function calculates the divisors by O(N^(1/2)) approach and also maintains the ascending order
+vector<ll> printDivisors(ll n)
+{
+    vector<ll>v;
+    ll i;
+    for (i = 1; i * i < n; i++) {
+        if (n % i == 0)
+            v.push_back(i);
+    }
+    if (i - (n / i) == 1) {
+        i--;
+    }
+    for (; i >= 1; i--) {
+        if (n % i == 0)
+            v.push_back(n/i);
+    }
+    return v; // returns the second largest element in the vector
+}
+void solve(){
+    string str;
+    int zerof=0,onef=0;
+    cin>>str;
+    for(auto i:str){
+        if(i=='1'){
+            onef++;
+        }else{
+            zerof++;
+        }
+    }
+    if(zerof==1||onef==1) cout<<"Yes"<<"\n";
+    else cout<<"No"<<"\n";
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
