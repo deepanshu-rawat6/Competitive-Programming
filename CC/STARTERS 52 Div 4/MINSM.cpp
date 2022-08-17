74539ca1d654341e68bdd4eb4ec11af7ab62658e
Deepanshu Rawat <deepanshu.dr10@gmail.com>
2022-08-17 21:45:59 +0530

    CC Starters 52 div 4

---
+++ CC/STARTERS 52 Div 4/MINSM.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define pb push_back
+#define pp pop_back
+#define Vec vector<long long int>
+const long long int MOD = 1e9+7;
+// Prime function with O(N^(1/2)) approach
+bool prime(int n){
+    int i=2;
+    if(n<2){
+        return false;
+    }
+    while(i*i<=n){
+        if(n%i==0){
+            return false;
+        }
+        i++;
+    }
+    return true;
+}
+// This function calculates the divisors by O(N^(1/2)) approach and also maintains the ascending order
+vector<ll> printDivisors(ll n)
+{
+    vector<ll>v;
+    ll i;
+    for (i = 1; i * i < n; i++) {
+        if (n % i == 0)
+            v.push_back(i);
+    }
+    if (i - (n / i) == 1) {
+        i--;
+    }
+    for (; i >= 1; i--) {
+        if (n % i == 0)
+            v.push_back(n/i);
+    }
+    return v; // returns the second largest element in the vector
+}
+int gcd(int a,int b){
+    if(b==0) return a;
+    return gcd(b%a,a);
+}
+void solve(){
+    int n;
+    cin>>n;
+    vector<ll>v(n);
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+    sort(v.begin(),v.end());
+    int a=v[0];
+    for(int i=1;i<n;i++){
+        v[i]=gcd(a,v[i]);
+    }
+    int sum=0;
+    cout<<accumulate(v.begin(),v.end(),sum)<<"\n";
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
