3b3991b6fe38ac514944f39ea6af769c52b5b017
Deepanshu Rawat <deepanshu.dr10@gmail.com>
2022-08-30 21:53:21 +0530

    adding codes

---
+++ Practice/KickStart/record_breaker.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define pb push_back
+#define pp pop_back
+#define Vec vector<long long int>
+const long long int MOD = 1e9+7;
+// Prime function with O(N^(1/2)) approach
+bool prime(int n){
+    int i=2;
+    if(n<2){
+        return false;
+    }
+    while(i*i<=n){
+        if(n%i==0){
+            return false;
+        }
+        i++;
+    }
+    return true;
+}
+// This function calculates the divisors by O(N^(1/2)) approach and also maintains the ascending order
+vector<ll> printDivisors(ll n)
+{
+    vector<ll>v;
+    ll i;
+    for (i = 1; i * i < n; i++) {
+        if (n % i == 0)
+            v.push_back(i);
+    }
+    if (i - (n / i) == 1) {
+        i--;
+    }
+    for (; i >= 1; i--) {
+        if (n % i == 0)
+            v.push_back(n/i);
+    }
+    return v; // returns the second largest element in the vector
+}
+ll gcd (ll a, ll b) {
+    return b ? gcd (b, a % b) : a;
+}
+ll solve(){
+    ll n;
+    cin>>n;
+    vector<ll> v(n);
+    for(ll i=0;i<n;i++){
+        cin>>v[i];
+    }
+    ll sum=v[0];
+    for(ll i=1;i<n-1;i++){
+        
+    }
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    for(int i=1;i<=t;i++){
+        cout<<"Case #"<<i<<": "<<solve();
+    }
+    return 0;
+}
